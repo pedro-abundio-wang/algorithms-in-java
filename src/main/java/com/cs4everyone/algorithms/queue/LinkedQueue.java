47aa4426352f35900b12d4852cf9ee04180ba4b4
pedro-abundio-wang <pedro.abundio.wang@gmail.com>
2021-01-11 19:55:06 +0800

    queue

---
+++ src/main/java/com/cs4everyone/algorithms/queue/LinkedQueue.java
@@ -0,0 +1,86 @@
+package com.cs4everyone.algorithms.queue;
+
+import com.cs4everyone.algorithms.stack.LinkedStack;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedQueue<T> implements Iterable<T> {
+
+  private LinkedNode head;
+
+  private LinkedNode tail;
+
+  private int size;
+
+  private class LinkedNode {
+    private T item;
+    private LinkedNode next;
+  }
+
+  private class LinkedIterator implements Iterator<T> {
+
+    private LinkedNode current;
+
+    public LinkedIterator(LinkedNode current) {
+      this.current = current;
+    }
+
+    public boolean hasNext() {
+      return current != null;
+    }
+
+    public T next() {
+      if (!hasNext()) {
+        if (!hasNext()) throw new NoSuchElementException();
+      }
+      T item = current.item;
+      current = current.next;
+      return item;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException("remove");
+    }
+  }
+
+  public T dequeue() {
+    if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+    T item = head.item;
+    head = head.next;
+    this.size--;
+    if (isEmpty()) {
+      tail = null;
+    }
+    return item;
+  }
+
+  public T peek() {
+    if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+    return head.item;
+  }
+
+  public void enqueue(T item) {
+    LinkedNode tailBackup = tail;
+    tail = new LinkedNode();
+    tail.item = item;
+    if (isEmpty()) {
+      head = tail;
+    } else {
+      tailBackup.next = tail;
+    }
+    this.size++;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public Iterator<T> iterator() {
+    return new LinkedIterator(head);
+  }
+}
---
+++ src/main/java/com/cs4everyone/algorithms/queue/ResizingArrayQueue.java
@@ -0,0 +1,99 @@
+package com.cs4everyone.algorithms.queue;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ResizingArrayQueue<T> implements Iterable<T> {
+
+  private static final int INIT_CAPACITY = 8;
+
+  private int head;
+
+  private int tail;
+
+  private int size;
+
+  private T[] array;
+
+  public ResizingArrayQueue() {
+    head = 0;
+    tail = 0;
+    size = 0;
+    array = (T[]) new Object[INIT_CAPACITY];
+  }
+
+  public T dequeue() {
+    if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+    T item = array[head];
+    head = (head++) % array.length;
+    this.size--;
+    if (size < array.length / 4) {
+      // resize array
+      resize(array.length / 2);
+    }
+    return item;
+  }
+
+  public T peek() {
+    if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+    T item = array[head];
+    array[head] = null;
+    return item;
+  }
+
+  public void enqueue(T item) {
+    array[tail] = item;
+    tail = (tail++) % array.length;
+    this.size++;
+    if (head == tail) {
+      // resize array
+      resize(2 * array.length);
+    }
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  private void resize(int capacity) {
+    T[] copy = (T[]) new Object[capacity];
+    for (int i = 0; i < this.size; i++) {
+      copy[i] = array[(head + i) % array.length];
+    }
+    head = 0;
+    tail = size;
+    array = copy;
+  }
+
+  public Iterator<T> iterator() {
+    return new ArrayIterator(head);
+  }
+
+  private class ArrayIterator implements Iterator<T> {
+
+    public int current;
+
+    public ArrayIterator(int current) {
+      this.current = current;
+    }
+
+    public boolean hasNext() {
+      return current < tail || current > head;
+    }
+
+    public T next() {
+      if(!hasNext()) throw new NoSuchElementException();
+      T item = array[current];
+      current = (current++) % array.length;
+      return item;
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException("remove");
+    }
+  }
+}
