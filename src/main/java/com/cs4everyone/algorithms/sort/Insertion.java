0b68d75adbd03c22030e75c6beba84a466cfc4a2
pedro-abundio-wang <pedro.abundio.wang@gmail.com>
2021-02-01 14:16:58 +0800

    insert sort

--- src/main/java/com/cs4everyone/algorithms/sort/Insertion.java
+++ src/main/java/com/cs4everyone/algorithms/sort/Insertion.java
@@ -2,17 +2,25 @@ package com.cs4everyone.algorithms.sort;
 
 public class Insertion {
 
-    public static void sort(Comparable[] array) {
-        
+  public static void sort(Comparable[] array) {
+    for (int i = 0; i < array.length; i++) {
+      for (int j = i + 1; j > 0; j--) {
+        if (less(array[j], array[j - 1])) {
+          swap(array, j, j - 1);
+        } else {
+          break;
+        }
+      }
     }
+  }
 
-    private static boolean less(Comparable a, Comparable b) {
-        return a.compareTo(b) < 0;
-    }
+  private static boolean less(Comparable a, Comparable b) {
+    return a.compareTo(b) < 0;
+  }
 
-    private static void swap(Comparable[] array, int i, int j) {
-        Comparable swap = array[i];
-        array[i] = array[j];
-        array[j] = swap;
-    }
+  private static void swap(Comparable[] array, int i, int j) {
+    Comparable swap = array[i];
+    array[i] = array[j];
+    array[j] = swap;
+  }
 }
