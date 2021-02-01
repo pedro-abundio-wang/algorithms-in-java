be810196162cd99c89736e26ed53595a251304da
pedro-abundio-wang <pedro.abundio.wang@gmail.com>
2021-02-01 16:06:25 +0800

    shell sort

--- src/main/java/com/cs4everyone/algorithms/sort/Shell.java
+++ src/main/java/com/cs4everyone/algorithms/sort/Shell.java
@@ -2,9 +2,30 @@ package com.cs4everyone.algorithms.sort;
 
 public class Shell {
 
-    public static void sort(Comparable[] array) {}
+  public static void sort(Comparable[] array) {
 
-    private static void less(Comparable a, Comparable b) {}
+    int h = 1;
+    while (h < array.length / 3) h = 3 * h + 1;
 
-    private static void swap(Comparable[] array, int i, int j) {}
+    while (h >= 1) {
+      for (int i = h; i < array.length; i++) {
+        for (int j = i; j >= h; j = j - h) {
+          if (less(array[j], array[j - h])) {
+            swap(array, j, j - h);
+          }
+        }
+      }
+      h = h / 3;
+    }
+  }
+
+  private static boolean less(Comparable a, Comparable b) {
+    return a.compareTo(b) < 0;
+  }
+
+  private static void swap(Comparable[] array, int i, int j) {
+    Comparable swap = array[i];
+    array[i] = array[j];
+    array[j] = swap;
+  }
 }
